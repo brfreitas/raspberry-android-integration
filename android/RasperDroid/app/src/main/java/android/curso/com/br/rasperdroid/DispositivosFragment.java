692e1b46824a9bc8e77b1fc619d701d1bd33b740
baff <baff@cin.ufpe.br>
2015-08-28 15:36:46 -0300

    menu

---
+++ android/RasperDroid/app/src/main/java/android/curso/com/br/rasperdroid/DispositivosFragment.java
@@ -0,0 +1,55 @@
+package android.curso.com.br.rasperdroid;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link DispositivosFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link DispositivosFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DispositivosFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static DispositivosFragment newInstance(int sectionNumber) {
+        DispositivosFragment fragment = new DispositivosFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public DispositivosFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_dispositivos, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
